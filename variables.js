edd573e3ecaa17e02271d096c9cceb03aa435330
Luyanda <luyandakwazi16@gmail.com>
2025-04-10 15:56:37 +0200

    I already had node installed

---
+++ variables.js
@@ -0,0 +1,16 @@
+//Exercise 1
+
+let name = "Luyanda Khawula";
+let age = 23;
+let isStudent = false;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+//Exercise 2
+let favouriteColour = "Navy Blue";
+console.log(favouriteColour);
+
+favouriteColour = "Black";
+console.log(favouriteColour);
