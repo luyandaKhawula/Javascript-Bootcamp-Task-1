f22db2e0428570ee5ff9e98a201a722f7a7efb60
Luyanda <luyandakwazi16@gmail.com>
2025-04-10 16:03:22 +0200

    Completed variables exercises

--- variables.js
+++ variables.js
@@ -10,7 +10,21 @@ console.log(isStudent);
 
 //Exercise 2
 let favouriteColour = "Navy Blue";
-console.log(favouriteColour);
+console.log("My favourite colour is " + favouriteColour);
 
 favouriteColour = "Black";
-console.log(favouriteColour);
+console.log("My new favourite colour is " + favouriteColour);
+
+//Exercise 3
+let num1 = 7;
+let num2 = 23;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+console.log("Sum: " + sum);
+console.log("Difference: " + difference);
+console.log("Product: " + product);
+console.log("Quotient: " + quotient);
